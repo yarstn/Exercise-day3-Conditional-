d1ac2e5db2435af458d3408e467fc085d222258d
yara <54339485+yarstn@users.noreply.github.com>
2024-07-02 13:29:51 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,147 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//        1.Write a program that checks the role of the user
+//        If the role is admin print "welcome admin"
+//        If the role is superuser print "welcome superuser"
+//        If the role is user print "welcome user" (tip:
+//        use if else)
+        Scanner input = new Scanner(System.in);
+//        String role;
+//        String role1="admin";
+//        String role2="superuser";
+//        String role3="user";
+//
+//       System.out.println("please enter your role: ");
+//        role = input.nextLine();
+//        if( role.equals(role1)){
+//            System.out.println("welcome admin");
+//        }
+//        else if( role.equals(role2)){
+//            System.out.println("welcome superuser");
+//        }  else if( role.equals(role3)){
+//            System.out.println("welcome user");
+//        }
+//        2.Take three numbers from the user and print the greatest number.
+//                Test Data
+//        Input the 1st number: 25
+//        Input the 2nd number: 78
+//        Input the 3rd number: 87
+//        Expected Output : The
+//        greatest: 87
+//        int no;
+//        int no2;
+//        int no3;
+//        System.out.println("please enter NO 1: ");
+//        no = input.nextInt();
+//        System.out.println("please enter NO 2: ");
+//        no2 = input.nextInt();
+//        System.out.println("please enter NO 3: ");
+//        no3 = input.nextInt();
+//
+//        if( no >= no2 && no >= no3){
+//            System.out.println(no);
+//        }
+//        else if(no2 >= no && no2 >= no3){
+//            System.out.println(no2);
+//        }  else if(no3 >= no2 && no3 >= no){
+//            System.out.println(no3);
+//        }
+
+//        3.Write a Java program that generates an integer between 1 and 7 and
+//        displays the name of the weekday.
+//        Test Data
+//        number: 4
+//        Expected Output:
+//        Wednesday
+//int day;
+//        System.out.println("enter between 1 and 7 to displays the name of the weekday ");
+//        day = input.nextInt();
+//        switch (day){
+//            case 1:
+//                System.out.println("Sunday");
+//                break;
+//            case 2:
+//                System.out.println("Monday");
+//                break;
+//            case 3:
+//                System.out.println("Tuseday");
+//                break;
+//            case 4:
+//                System.out.println("Wenesday");
+//                break;
+//            case 5:
+//                System.out.println("Thursday");
+//                break;
+//            case 6:
+//                System.out.println("Fraiday");
+//                break;
+//            case 7:
+//                System.out.println("Saturday");
+//                break;
+//        }
+//        4. Write a program that takes a numeric score as input and prints the
+//        corresponding letter grade using the following grading scale:
+//        A: 90-100
+//        B: 80-89
+//        C: 70-79
+//        D: 60-69
+//        F: 0-59
+//        Enter your numeric score: 85
+//        Numeric Score: 85
+//        Letter Grade: B
+//        int dgree;
+//        System.out.println("enter your dgree ");
+//        dgree = input.nextInt();
+//        if( dgree >100){
+//            System.out.println("Fail");
+//        }
+//        else if(dgree >= 95  ){
+//            System.out.println("A+");
+//        }
+//        else if (dgree >= 90 ){
+//            System.out.println("A");
+//        } else if (dgree >= 85){
+//            System.out.println("B+");
+//        } else if (dgree >= 80){
+//            System.out.println("B");
+//        } else if (dgree >= 75){
+//            System.out.println("C+");
+//        } else if (dgree >= 70){
+//            System.out.println("C");
+//        } else if (dgree >= 65){
+//            System.out.println("D+");
+//        } else if (dgree >= 60){
+//            System.out.println("D");
+//        } else{
+//            System.out.println("F");
+//        }
+//        5. Write a Java program that takes a person's age as input and
+//        categorizes them into one of three age categories: "Child,"
+//        "Teenager," or "Adult" using an if statement.
+//                use an if statement to categorize the age based on the following criteria:
+//• If the age is less than 13, categorize them as a "Child."
+//• If the age is between 13 and 19 (inclusive), categorize them as a
+//        "Teenager."
+//• If the age is 20 or older, categorize them as an "Adult."
+//        Sample Output:
+//        Enter your age: 25
+//        You are an Adult.
+//        int age;
+//        System.out.println("enter your age ");
+//        age = input.nextInt();
+//        if (age <= 13) {
+//            System.out.println("you are child");
+//        }
+//            else if (age > 13 && age <= 19)
+//         {
+//            System.out.println("inclusive");
+//        }
+//            else if(age > 20 ) {
+//            System.out.println("you are adult");
+//        }
+//
+    }}
